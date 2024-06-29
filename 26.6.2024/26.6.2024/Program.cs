49a4b0e299ae78e216319cd5047a09d4346262d7
hmoodeh98 <hmoodeh9820@gmail.com>
2024-06-29 15:56:07 +0300

    27.6.2024

--- 26.6.2024/26.6.2024/Program.cs
+++ 26.6.2024/26.6.2024/Program.cs
@@ -11,7 +11,7 @@ namespace _26._6._2024
         static void Main(string[] args)
         {
             ///////Task 1
-            /*  Console.WriteLine("Enter the first integer:");
+              Console.WriteLine("Enter the first integer:");
                 string input1 = Console.ReadLine();
                 Console.WriteLine("Enter the second integer:");
                 string input2 = Console.ReadLine();
@@ -24,22 +24,33 @@ namespace _26._6._2024
                 Array.Sort(num);
                 Console.WriteLine($"The smaller number is:  {num[0]} ");
 
-            *
+                Console.WriteLine();
                 //////////Task 2
-                Console.WriteLine("Enter a number:");
-                int number = int.Parse(Console.ReadLine());
-                if (number < 0)
-                {
-                    Console.WriteLine("The sign is -");
-                }
-                else 
-                { 
-                    Console.WriteLine("The sign is +");
-                }
+                //Console.WriteLine("Enter a number:");
+                //int number = int.Parse(Console.ReadLine());
+                //if (number < 0)
+                //{
+                //    Console.WriteLine("The sign is -");
+                //}
+                //else 
+                //{ 
+                //    Console.WriteLine("The sign is +");
+                //}
+           
 
-            *
+                int num3 = 7;
+                int num4 = -24;
+                string sign3 = num3.ToString("+#;-#;0");
+                string sign4 = num4.ToString("+#;-#;0");
+                Console.WriteLine("the sign of 7 is " + sign3[0]);
+                Console.WriteLine("the sign of -24 is " + sign4[0]);
 
+            
+            Console.WriteLine();
                 ///////Task 3
+                ///
+                Console.WriteLine("Enter three numbers9" +
+                    ":");
                 int number1 = int.Parse(Console.ReadLine());
                 int number2 = int.Parse(Console.ReadLine());
                 int number3 = int.Parse(Console.ReadLine());
@@ -47,25 +58,25 @@ namespace _26._6._2024
                 Array.Sort(arr);
                 Console.WriteLine($"{arr[2]}, {arr[1]}, {arr[0]}");
 
-            *
 
+                Console.WriteLine();
                 ///////Task 4
                 ///
                 int[] ints = { 1, 2, 3, 4, 5 };
                 Array.Sort(ints);
                 Console.WriteLine($"The maximum number is :{ints[4]}");
 
-            *
 
+                Console.WriteLine();
                 ///////Task 5
                 ///
                 Console.WriteLine("Input kilometers per hour: ");
                 int km = int.Parse(Console.ReadLine());
                 double m = km * 0.625;
                 Console.WriteLine($"{m} miles per hour");
-            *
 
 
+                Console.WriteLine();
                 ///////Task 6
                 Console.WriteLine("Enter the number of hours:");
                 int hours = int.Parse(Console.ReadLine());
@@ -76,29 +87,29 @@ namespace _26._6._2024
 
                 int totalMinutes = (hours * 60) + minutes;
                 Console.WriteLine("The total number of minutes is: " + totalMinutes);
-            *
 
 
 
+                Console.WriteLine();
                 ///////Task 7
                 ///
                 Console.WriteLine("Enter the number of minutes:");
-                int minutes = int.Parse(Console.ReadLine());
-                int hours = minutes / 60;
-                int seconds = (minutes % 60);
-                Console.WriteLine($"{hours} Hours, {seconds} Minutes");
+                int minute = int.Parse(Console.ReadLine());
+                int hour = minute / 60;
+                int seconds = (minute % 60);
+                Console.WriteLine($"{hour} Hours, {seconds} Minutes");
 
-            */
 
-            ////////Task 8
-            ///
-            string[] sentence = { "hello world", "Orang Academy", "Mohammad algharibeh", "wwwwwwwwwww", "nnnnnnnnnnnnnn" };
-            Console.WriteLine($"the sentence {sentence[0]} his length {sentence[0].Length}");
-            Console.WriteLine($"the sentence {sentence[1]} his length {sentence[1].Length}");
-            Console.WriteLine($"the sentence {sentence[2]} his length {sentence[2].Length}");
-            Console.WriteLine($"the sentence {sentence[3]} his length {sentence[3].Length}");
-            Console.WriteLine($"the sentence {sentence[4]} his length {sentence[4].Length}");
-
+                Console.WriteLine();
+                ////////Task 8
+                ///
+                string[] sentence = { "hello world", "Orang Academy", "Mohammad algharibeh", "wwwwwwwwwww", "nnnnnnnnnnnnnn" };
+                Console.WriteLine($"the sentence {sentence[0]} his length {sentence[0].Length}");
+                Console.WriteLine($"the sentence {sentence[1]} his length {sentence[1].Length}");
+                Console.WriteLine($"the sentence {sentence[2]} his length {sentence[2].Length}");
+                Console.WriteLine($"the sentence {sentence[3]} his length {sentence[3].Length}");
+                Console.WriteLine($"the sentence {sentence[4]} his length {sentence[4].Length}");
+            
 
         }
     }
---
+++ 27.06.2024/27.06.2024/Program.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _27._06._2024
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //1 - Correct the syntax error:
+
+            //	string[] ARR = [1, 7  9  45,]
+            //	int arr2 = ["Str" "alex","moh"
+            //	string arr3= 'the','fox' 'over' lazy, 'dog',  ]
+            //	
+            int[] ARR = { 1, 7, 9, 45 }; //Changed data type to integer, Change braces to curly braces, adding commas and semicolons.
+
+            string[] arr2 = { "Str", "alex", "moh" }; //Changed data type to string and definition of array, Change braces to curly braces, adding commas and semicolons.
+
+            string[] arr3 = { "the", "fox", "over", "lazy", "dog" };//Changed data type to string and definition of array, Change braces to curly braces and change single quotaion to double quotaion.
+
+
+
+            //2- What the index of "Banana","Tomato"?
+
+            String[] fruits = { "Tomato", "Banana", "Watermelon" };
+
+            int indexBanana = Array.IndexOf(fruits, "Banana");
+            int indexTomato = Array.IndexOf(fruits, "Tomato");
+
+            Console.WriteLine("Index of Banana: " + indexBanana);
+            Console.WriteLine("Index of Tomato: " + indexTomato);
+
+
+            Console.WriteLine();
+            Console.WriteLine("----------------------------------------------------------------");
+            Console.WriteLine();
+            //3-	Create an multiple arrays that represents your:
+            //        	Favorite Food(5 item)
+            //        	Favorite Sport(3 item)
+            //        	Favorite Movie(4 item)
+            //         Then, print each array using foreach, and Loop Through an Array
+
+            string[] favoriteFood = { "Pizza", "Shawarma", "Lazania", "Burger", "Mansaf" };
+            string[] favoriteSport = { "Footboll", "Fifa", "Playstation" };
+            string[] favoriteMovie = { "Black Panther", "interstaler", "Joker", "One piece" };
+
+            foreach (string food in favoriteFood)
+            {
+                Console.WriteLine(food);
+            }
+            Console.WriteLine();
+            foreach (string sport in favoriteSport)
+            {
+                Console.WriteLine(sport);
+            }
+            Console.WriteLine();
+            foreach (string movie in favoriteMovie)
+            {
+                Console.WriteLine(movie);
+            }
+            Console.WriteLine();
+            Console.WriteLine("using loop:");
+            int i = 0;
+            while (i < favoriteFood.Length)
+            {
+                Console.WriteLine(favoriteFood[i]);
+                i++;
+            }
+            Console.WriteLine();
+            int j = 0;
+            while (j < favoriteSport.Length)
+            {
+                Console.WriteLine(favoriteSport[j]);
+                j++;
+            }
+            Console.WriteLine();
+            int h = 0;
+            while (h < favoriteMovie.Length)
+            {
+                Console.WriteLine(favoriteMovie[h]);
+                h++;
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("----------------------------------------------------------------");
+            Console.WriteLine();
+            ////////////4-	Write a program in C# to calculate the sum of three numbers with getting 
+            ///input in one line separated by a comma
+            ///
+            Console.Write("Input three numbers separated by comma: ");
+            string input = Console.ReadLine();
+
+            string[] numbers = input.Split(',');
+
+            int num1 = int.Parse(numbers[0]);
+            int num2 = int.Parse(numbers[1]);
+            int num3 = int.Parse(numbers[2]);
+
+            int sum = num1 + num2 + num3;
+
+            Console.WriteLine($"The sum of three numbers: {sum}");
+
+            Console.WriteLine();
+            Console.WriteLine("----------------------------------------------------------------");
+            Console.WriteLine();
+            ///5-	Write a program in C# to display the n terms of odd number and their sum from [1- 100 ]. 
+            ///
+            string s = "";
+            int sums = 0;
+            for (int n = 0; n < 100; n++)
+            {
+                if (n % 2 != 0)
+                {
+                    s += $" {n}";
+                    sums += n;
+                }
+            }
+            Console.WriteLine($"The odd numbers are {s}");
+            Console.WriteLine($"The Sum of odd Numbers is: {sums}");
+
+             //6 - Write a program in C## to display the pattern like right angle triangle using an asterisk. Go to the editor
+            //
+            for (int q = 1; q <= 4; q++)
+            {
+                for (int w = 1; w <= q; w++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+
+            ////- Write a program in C# to make such a pattern like right angle triangle with number increased by 1. Go to the editor
+            /////
+            int v = 1;
+            for (int q = 1; q <= 4; q++)
+            {
+                for (int w = 1; w <= q; w++)
+                {
+                    Console.Write(v);
+                    v++;
+                }
+                Console.WriteLine();
+            }
+            
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
